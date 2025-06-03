63d3b963bdf75e5c8845cd42bca7f419469de6e1
Andipascale7 <andipascale7@outlook.com>
2025-06-03 13:03:51 +0100

    working expo version

---
+++ app/components/Header.tsx
@@ -0,0 +1,58 @@
+import React from "react"
+import {View, Text, StyleSheet, Image} from "react-native"
+import { useFonts} from 'expo-font'
+
+// const loadFonts = async () => {
+//   await Font.loadAsync({
+//     'Playwrite': require('./assets/fonts/Playwrite.ttf')
+//   })
+// }
+
+function Header() {
+  const [fontsLoaded] = useFonts({
+    'Playwrite': require('../assets/fonts/Playwrite_HU/PlaywriteFont.ttf')
+  })
+
+ 
+return (
+    <View style={styles.headerContainer}>
+      <View style={styles.logoContainer}>
+    <Image source={require('../assets/images/giggle-logo.png')} style={styles.logo} 
+    resizeMode="contain"
+    /> 
+    </View>
+    <Text style={styles.headerTitle}>
+      Giggle
+    </Text>
+    </View>
+)
+}
+export default Header
+
+const styles=StyleSheet.create({
+  headerContainer: {
+    backgroundColor: '#bdfcc9',
+    width: '90%',
+    alignItems: 'center',
+    borderRadius: 20,
+    margin: 15,
+    padding: 30,
+
+  },
+  headerTitle: {
+    fontSize: 40,
+    marginRight: 10,
+    fontFamily: 'Playwrite'
+
+  },
+  logo: {
+    width: 150,
+    height: 75,
+  },
+  logoContainer: {
+    flexDirection: 'column',
+    position: 'absolute',
+    top: 20,
+    right: 20,
+  }
+})
