902a77a8ac42f5544da8af02851af1d1f4f78554
Uzo Ugochukwu <uugochukwu@aol.com>
2025-06-04 09:51:38 +0100

    NavBar tag issues fixed

--- Giggle/app/components/NavBar.tsx
+++ Giggle/app/components/NavBar.tsx
@@ -1,40 +1,40 @@
-import { NavigationContainer, NavigationIndependentTree } from "@react-navigation/native";
+import {
+  NavigationContainer,
+  NavigationIndependentTree,
+} from "@react-navigation/native";
 import HomePage from "./HomePage";
 import Chat from "./Chat";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StyleSheet } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
+const Stack = createNativeStackNavigator();
 
-
-const Stack = createNativeStackNavigator()
-
-const Tab = createBottomTabNavigator()
+const Tab = createBottomTabNavigator();
 
 export default function NavBar() {
-    return (<NavigationIndependentTree>
-<NavigationContainer>
-
-    
-    
-        <Tab.Navigator initialRouteName={HomePage} />
-        <Tab.Screen name="HomePage" component={HomePage} />
-        <Tab.Screen name="Chat" component={Chat} />
-
-
-</NavigationContainer></NavigationIndependentTree>
-    )
+  return (
+    <NavigationIndependentTree>
+      <NavigationContainer>
+        <Tab.Navigator initialRouteName="HomePage">
+          <Tab.Screen name="HomePage" component={HomePage} />
+          <Tab.Screen name="Chat" component={Chat} />
+          {/* <Tab.Screen name="User Profile" component={UserProfile} /> */}
+        </Tab.Navigator>
+      </NavigationContainer>
+    </NavigationIndependentTree>
+  );
 }
 
-const styles=StyleSheet.create({
-  NavBar:{
+const styles = StyleSheet.create({
+  NavBar: {
     alignItems: "center",
     padding: 20,
-    backgroundColor: '#bdfcc9',
-    width: '100%',
-    justifyContent: 'center',
+    backgroundColor: "#bdfcc9",
+    width: "100%",
+    justifyContent: "center",
   },
   link: {
-    textDecorationLine: 'underline'
-  }
-})
+    textDecorationLine: "underline",
+  },
+});
