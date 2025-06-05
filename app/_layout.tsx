a385458ce2870d239277235484028d8c03330b76
dnesgurung <174604785+dnesgurung@users.noreply.github.com>
2025-06-05 19:40:28 +0100

    fixed react native version mismatch

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,25 @@
-import { Stack } from "expo-router"
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context"
+import { Stack } from "expo-router";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{flex: 1, backgroundColor: '#000'}}>
-        <Stack screenOptions={{headerShown: false}}>
-          <Stack.Screen name="index" options={{title: "Home"}}/>
-          <Stack.Screen name="Wishlist" options={{title: "Wishlist"}}/>
-          <Stack.Screen name="Messages" options={{title: "Messages"}}/>
-          <Stack.Screen name="Profile" options={{title: "Profile"}}/>
+      <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }}>
+        <Stack
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: "#ADC178",
+            },
+          }}
+        >
+          <Stack.Screen name="index" options={{ title: "Home" }} />
+          <Stack.Screen name="Wishlist" options={{ title: "Wishlist" }} />
+          <Stack.Screen name="Messages" options={{ title: "Messages" }} />
+          <Stack.Screen name="Profile" options={{ title: "Profile" }} />
         </Stack>
       </SafeAreaView>
     </SafeAreaProvider>
-  )
+  );
 }
 
-//settings need to be inside profile stacklist 
+//settings need to be inside profile stacklist
