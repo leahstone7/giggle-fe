f93f95c2881192b78bd280d9105d6fc0bdb9861d
David Potter <davidgeorgepotter@protonmail.com>
2025-06-09 13:28:55 +0100

    Add newEvent tab and ticketmasterSearchBar component

---
+++ app/(tabs)/newEvent.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { Text, View } from "react-native";
+import { styles } from "../../styles/auth.styles";
+import SearchTMEvents from "../components/TicketmasterSearchBar";
+
+const [tmEvents, setTmEvents] = useState([]);
+
+//search for new event serachbar WITH A SEARCH BUTTON
+//calls ticketmaster USEEFFECT ONCE ONCE ONCE ONCE ONCE  BASED ON SEARCH BUTTON
+//click it as a link to...add?
+//automatically posts to our server
+//thanks, event added!
+console.log(tmEvents)
+
+export default function newEvent() {
+  return (
+    <View style={styles.container}>
+      <Text>Couldn't find the event you were looking for? Add it here!</Text>
+      <Text>
+        (This page will be moved off the navbar and accessible through a link on
+        the homepage)
+      </Text>
+      <SearchTMEvents setTmEvents={setTmEvents} /> 
+  {/* map through events and map the details, plus a button for add */}
+    </View> 
+  );
+}
---
+++ app/components/TicketmasterSearchBar.tsx
@@ -0,0 +1,53 @@
+import { Ionicons } from "@expo/vector-icons";
+import React from "react";
+import { Button, StyleSheet, TextInput, View } from "react-native";
+import { getTMEventsByKeyword } from "../../utils/api";
+
+function SearchTMEvents({ setTmEvents}) {
+  const [searchQuery, setSearchQuery] = React.useState("");
+
+//UPDAET SEARCH WUERY ON CHANGE TEXT HERE IN FUNCTION INSTEAD OF IN RETURN LINE 24
+
+  const searchTM = () => {
+    getTMEventsByKeyword(searchQuery).then((events) => {
+      console.log(events);
+      setTmEvents(events);
+    });
+  };
+
+  return (
+    <View style={styles.searchSectionWrapper}>
+      <View style={styles.searchBar}>
+        <Ionicons name="search" size={20} style={{ marginRight: 5 }} />
+        <TextInput
+          placeholder="Search for new events ..."
+          onChangeText={(e) => setSearchQuery(e)}
+        />
+      </View>
+      <View>
+        <Button title="Search" color="#f194ff" onPress={searchTM} />
+      </View>
+    </View>
+  );
+}
+export default SearchTMEvents;
+
+const styles = StyleSheet.create({
+  searchSectionWrapper: {
+    flexDirection: "row",
+  },
+  searchBar: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "#efefef",
+    padding: 16,
+    borderRadius: 10,
+    width: 300,
+  },
+
+  filterBtn: {
+    padding: 12,
+    borderRadius: 10,
+    backgroundColor: "#efefef",
+  },
+});
