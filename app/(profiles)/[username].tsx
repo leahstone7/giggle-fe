007c1ed06f145fd607cf6bd4e67d817c84b70c85
dnesgurung <174604785+dnesgurung@users.noreply.github.com>
2025-06-11 21:23:17 +0100

    link to interested events fixed

--- app/(profiles)/[username].tsx
+++ app/(profiles)/[username].tsx
@@ -2,7 +2,7 @@ import { IUser } from "@/context/userContext";
 import { getEventById, getUserByUserName } from "@/utils/api";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import React, { useEffect, useState } from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import Loader from "../components/loader";
 
 export default function UserDetails() {
@@ -164,18 +164,25 @@ export default function UserDetails() {
         <Text style={styles.biography}>
           {user.biography || "No biography provided"}
         </Text>
-      </View>
 
+      </View>
       {user.interestedEvents.length > 0 && (
-        <View style={styles.section}>
+          <View style={styles.section}>
           <Text style={styles.sectionTitle}>Interested Events</Text>
           <View style={styles.eventsContainer}>
             {user.interestedEvents.map((eventId, index) => (
-
-          <View key={index} style={styles.event}>
+               
+                <View key={index} style={styles.event}>
+                    <TouchableOpacity onPress={()=>{
+                      router.push({
+                          pathname: '/(events)/[eventId]',
+                          params:{eventId: eventId }
+                      })
+                    }}>
                 <Text style={{ fontWeight: "bold" }}>
                 {/* <Text>{formatDate(event.date)}</Text> */}
                 {eventNames[eventId] || "Unknown Event"}</Text>
+            </TouchableOpacity>
               </View>
             ))}
           </View>
